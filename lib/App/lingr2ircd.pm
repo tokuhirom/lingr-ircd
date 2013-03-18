f713554e4039bc908b59785efd99f87d32939662
tokuhirom <tokuhirom@gmail.com>
2013-03-18 15:34:02 +0900

    initial import

---
+++ lib/App/lingr2ircd.pm
@@ -0,0 +1,41 @@
+package App::lingr2ircd;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.0.1';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+App::lingr2ircd - ...
+
+=head1 SYNOPSIS
+
+  use App::lingr2ircd;
+
+=head1 DESCRIPTION
+
+App::lingr2ircd is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
